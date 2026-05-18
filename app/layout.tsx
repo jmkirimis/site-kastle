9591b8e42f4e8282851d85950f22012ca0cd2165
João Marcos Alecsandro Kirimis <150600077+jmkirimis@users.noreply.github.com>
2026-05-18 05:58:30 -0300

    feat: adiciona integração com o Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const montserrat = Montserrat({
@@ -17,12 +18,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const gaId = process.env.NEXT_PUBLIC_GA_ID;
+
   return (
     <html lang="pt-br">
       <body
         className={`${montserrat.className} antialiased`}
       >
         {children}
+        {gaId && <GoogleAnalytics gaId={gaId} />}
       </body>
     </html>
   );
